da47f3e7cccbc8c06160fcb628fb2c95265a3ca0
TsinatKibru <110996940+TsinatKibru@users.noreply.github.com>
2024-08-24 12:09:15 +0300

    now using sqlite3

--- chat/settings.py
+++ chat/settings.py
@@ -89,23 +89,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'hms_test',
-        'USER': 'hms_user',
-        'PASSWORD': 'Hm@09552016',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'mysql.connector.django',
+#        'NAME': 'hms_test',
+#        'USER': 'hms_user',
+#        'PASSWORD': 'Hm@09552016',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+# }
 
 
 # Password validation
